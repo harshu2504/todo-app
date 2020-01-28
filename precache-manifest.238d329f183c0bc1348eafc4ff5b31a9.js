387fe3030c2cb2ab3064d781f9b04514eb2089cd
harsh solanki <hsolanki1884@gmail.com>
2020-01-28 09:07:52 +0530

    Updates

---
+++ precache-manifest.238d329f183c0bc1348eafc4ff5b31a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c38dd05ae51ab11d7c02f36c12be9b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "41a12f38e6380fda5865",
+    "url": "/todo-app/static/js/2.fe9213cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app/static/js/2.fe9213cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d64e59ae749ae3b1ee28",
+    "url": "/todo-app/static/js/main.ce9bc68f.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
